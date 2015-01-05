07b42898b55d4887f25d8a4863e5e839983d04ef
hangyan <yanhanglyf@163.com>
2015-01-05 15:34:19 +0800

    update

--- c/ds/intrusive-list/LinkList.h
+++ c/ds/intrusive-list/LinkList.h
@@ -29,6 +29,10 @@ If you have questions concerning this license or the applicable additional terms
 #ifndef __LINKLIST_H__
 #define __LINKLIST_H__
 
+
+#include <cstddef>
+
+
 /*
 ==============================================================================
 
---
+++ c/ds/intrusive-list/List.cpp
@@ -0,0 +1,16 @@
+#include <cstddef>
+#include "List.h"
+#include <iostream>
+
+class Foo {
+  LIST_LINK(Foo) link;
+  int data;
+};
+
+
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
