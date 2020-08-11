996530712b16e465b60d8ac3f65d392544500aa3
Hang Yan <hangyan@alauda.io>
2020-08-11 23:23:17 +0800

    Update

---
+++ alg/tree/binary-tree/main.go
@@ -0,0 +1,10 @@
+package main
+
+func preorderTraversal(root *TreeNode)  {
+    if root==nil{
+        return
+    }
+    // 先访问根再访问左右
+    fmt.Println(root.Val)
+    preorderTraversal(root.Right)
+}
